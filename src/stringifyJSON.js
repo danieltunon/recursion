87378689f81fc5fbb5fefe3cce63ab225cf63223
Danny Tunon <danieltunon@gmail.com>
2016-01-20 15:22:37 -0500

    Make /stringifyJSON/ pass tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,75 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function( obj ) {
+  var isValidObj = function( object ) {
+    var type = typeof object;
+    return ( type === 'object' &&
+             object !== null );
+  };
+
+  var isInvalidObj = function( object ) {
+    var type = typeof object;
+    return ( type === 'function' ||
+             type === 'undefined' );
+  };
+
+  var isValidNonObj = function( object ) {
+    var type = typeof object;
+    return ( object === null ||
+     ( type !== 'undefined' &&
+       type !== 'function' &&
+       type !== 'object' ) );
+  };
+
+  var stringer = function( obj ) {
+    var result = '';
+    var temp = [];
+    var temp2;
+    var skip;
+    if ( Array.isArray(obj) ) {
+      result += "[";
+      obj.forEach(function(e) {
+        if( isValidObj( e ) ) {
+          temp.push( stringer(e) );
+        } else if ( typeof e === 'string' ) {
+          temp.push('"' + e + '"');
+        } else {
+          temp.push(String(e));
+        }
+      });
+      result += temp.join();
+      result += "]";
+    } else if ( isValidObj(obj) ) {
+      result += "{";
+      var keys = Object.keys( obj );
+      keys.forEach( function( key ) {
+        temp2 = [('"' + key + '":')];
+        if ( isInvalidObj( obj[key] ) ) {
+          skip = true;
+        } else if( isValidObj( obj[key] ) ) {
+          temp2.push( stringer( obj[key] ) );
+        } else if ( typeof obj[key] === 'string' ) {
+          temp2.push('"' + obj[key] + '"');
+        } else {
+          temp2.push(String( obj[key] ));
+        }
+        temp.push(temp2.join(''));
+      });
+      result += temp.join();
+      result += "}";
+      if ( skip ) { result = '{}'; }
+    } else if ( isValidNonObj( obj ) ) {
+      if ( typeof obj === 'string' ) {
+        result += '"' + obj + '"';
+      } else {
+        result += String( obj );
+      }
+    }
+
+    return result;
+  };
+
+  return stringer( obj );
 };
+
