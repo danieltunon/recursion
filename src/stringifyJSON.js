5c6cb1abd353c56a02d4e7062a58d33abb7189df
Danny Tunon <danieltunon@gmail.com>
2016-01-20 23:14:54 -0500

    Refactor big time: remove lots of redundancy

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,80 +4,27 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function( obj ) {
-  var getType = function ( object ) {
-    var type = typeof object;
-    if ( Array.isArray( object ) ) {
-      return 'array';
-    } else if ( object !== null &&
-                type   === 'object' ) {
-      return 'object';
-    } else if ( type === 'string' ) {
-      return type;
-    } else if ( type   !== 'undefined' &&
-                type   !== 'function' ) {
-      return 'simpleObj';
-    } else {
-      return 'invalid';
-    }
-  };
+  var commaSeparator = [];
 
-  var quotify = function ( string ) {
-    return '"' + string + '"';
-  };
+  if ( obj === undefined || typeof obj === 'function' ) {
+    return undefined;
+  } else if ( typeof obj === 'string' ) {
+    return '"' + obj + '"';
+  } else if ( obj === null || typeof obj !== 'object' ) {
+    return String( obj );
+  } else if ( Array.isArray( obj ) ) {
+    obj.forEach( function(value) {
+      commaSeparator.push( stringifyJSON( value ) );
+    });
+    return "[" + commaSeparator.join() + "]";
+  } else {
+    var keys = Object.keys(obj);
+    keys.forEach( function(key) {
+      if ( obj[key] !== undefined && typeof obj[key] !== 'function' ) {
+        commaSeparator.push( '"' + key + '":' + stringifyJSON( obj[key] ) );
+      }
+    });
+    return "{" + commaSeparator.join() + "}";
+  }
 
-  var stringmogrify = function( obj ) {
-    var type = getType( obj );
-    var isValid = ( type !== 'invalid' );
-    var jsonString = '';
-    var commaSeparator = [];
-    var objectBuilder = '';
-    var valType;
-
-    if ( type === 'array' ) {
-      obj.forEach( function( value ) {
-        valType = getType( value );
-
-        if ( valType === 'invalid' ) {
-          // skip it
-        } else if ( valType === 'string' ) {
-          commaSeparator.push( quotify(value) );
-        } else if ( valType === 'simpleObj' ) {
-          commaSeparator.push( String( value ) );
-        } else {
-          commaSeparator.push( stringmogrify( value ) );
-        }
-      } );
-      jsonString += "[" + commaSeparator.join() + "]";
-    } else if ( type === 'object' ) {
-      var keys = Object.keys( obj );
-
-      keys.forEach( function( key ) {
-        valType = getType( obj[key] );
-        objectBuilder = quotify( key ) + ":";
-        if ( valType === 'invalid' ) {
-          // lets function skip it
-          objectBuilder = '';
-        } else if ( valType === 'string' ) {
-          objectBuilder += quotify( obj[key] );
-        } else if ( valType === 'simpleObj' ) {
-          objectBuilder += String( obj[key] );
-        } else {
-          objectBuilder += stringmogrify( obj[key] );
-        }
-        commaSeparator.push( objectBuilder );
-      } );
-      jsonString += "{" + commaSeparator.join() + "}";
-    } else if ( type === 'string' ) {
-      jsonString += quotify( obj );
-    } else if ( type === 'simpleObj' ){
-      jsonString += String( obj );
-    }
-
-    return jsonString;
-  };
-
-  return stringmogrify( obj );
 };
-
-
-stringifyJSON([]);
